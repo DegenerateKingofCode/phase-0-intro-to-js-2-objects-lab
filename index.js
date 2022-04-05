f7976d28276f78ec6e98b89b38e0db4b3beb856e
Tanner <Tanner@tleroy.com>
2022-04-05 17:44:56 -0400

    commit tree 415f42fd482b7d029f90dffde675cde46c0caa1b

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your solution in this file!
+const employee = {
+    name:"nameValue",
+    streetAdress: "addressValue"
+}
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newObj = {...employee};
+
+    newObj[key]= value;
+
+    return newObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+  
+    employee[key]= value;
+
+    return employee
+}
+function deleteFromEmployeeByKey(employee, key,){
+    const newObj2 = {...employee}
+
+    delete newObj2[key]
+
+    return newObj2
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key,){
+
+   delete employee[key]
+   
+   return employee
+}
